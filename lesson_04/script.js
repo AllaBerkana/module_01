67607e3eec8fc457b10d7df6716726bb5622720c
AllaBerkana <allaberkana@gmail.com>
2022-02-21 23:44:56 +0200

    Всё в Модуль 1

---
+++ lesson_04/script.js
@@ -0,0 +1,36 @@
+'use strict'
+
+
+/*
+const fruit = prompt ('Ведите название фрукта');
+const count = prompt ('Введите количество кг');
+const price = 50;
+
+const sum = count * price;
+
+switch(`Number.isNaN${count}`){
+    case true:
+        console.log('Вы ввели некорректные данные');
+        break;
+    default:
+        console.log(`Вы купили товар ${fruit} на ${count}кг. Сумма составляет ${sum} рублей`);
+        break;
+};
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lesson_04/task.js
@@ -0,0 +1,46 @@
+'use strict'
+
+/*
+
+//Вторая задача:
+{const rain = Math.round(Math.random());
+    console.log('rain: ', rain);
+    if (rain) {
+        console.log('Пошёл дождь. Возьмите зонт!');
+    } else {
+        console.log('Дождя нет!');
+    }
+}
+*/
+//Третья задача:
+/*
+{
+    const scoreMath = prompt('Введите кол-во баллов по математике:');
+    const scoreLang = prompt('Введите кол-во баллов по русскому языку:');
+    const scoreInformatics = prompt('Введите кол-во баллов по информатике:');
+    const sumScores = scoreMath + scoreLang + scoreInformatics;
+
+    if( sumScores >= 256){
+        console.log('Поздравляю, вы поступили на бюджет!');
+    } 
+
+}
+*/
+
+//Четвёртая задача:
+/*
+const money  = prompt("Сколько денег Вы хотите снять?");
+const minCash = 100;
+
+const remainder = money % 100;
+const cash  = money - remainder;
+
+if(money < 100){
+    console.log('Вы не можете снять деньги с карты');
+} else if (remainder === 0){
+    console.log('Можете снять всю сумму');
+} else {
+    console.log(`Вы можете снять ${cash}. Остаток на карточке ${remainder} рублей`);
+}
+
+*/
