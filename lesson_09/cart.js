1f25be66a65a66723247c3ee82e94a9bdce60c9c
Alla Berkana <allaberkana@gmail.com>
2022-08-18 13:54:06 +0300

    edit less 9

--- lesson_09/cart.js
+++ lesson_09/cart.js
@@ -1,105 +1,53 @@
 'use strict'
 
-{   /*
-    Создайте файл cart.js, подключите к html - файлу
-
-    Создайте объект cart — корзина
-
-    Объект будет содержать следующие свойства:
-
-    items = пустой массив - это товары
-    totalPrice = 0 - общая стоимость корзины
-    count = 0 - количество товаров 
-    
-    и методы
-
-    getTotalPrice - получить общую стоимость товаров
-    add - добавить товар
-    increaseCount - увеличить количество товаров
-    calculateItemPrice - посчитать общую стоимость товаров
-    clear - очистить корзину
-    print - распечатать корзину
-
-    Далее описание каждого метода
-
-    getTotalPrice()
-    метод возвращает значение свойства totalPrice
-
-    calculateItemPrice()
-    пересчитывает стоимость всей корзины и записывает значение в totalPrice 
-
-    increaseCount()
-    Принимает один параметр(число)
-
-    Увеличивает свойство count на это число
-
-    add()
-    Принимает три параметра:
-    название товара
-    цену товара
-    количество товара (опциональный параметр, по умолчанию 1 товар)
-    этот метод формирует объект из полученных параметров и добавляет его в свойство items
-
-    так же вызывает все необходимые методы чтобы свойства count и totalPrice были актуальные
-
-    clear()
-    Очищает полностью нашу корзину, возвращает все значения в изначальные
-
-    print()
-    Выводит в консоль JSON строку из массива items и на следующей строке выводит общую стоимость корзины
-
-    Для проверки работы функционала добавить 3 или более товаров в корзину
-    После вызвать метод print для вывода информации в консоль
-    */
-}
-
-
-//---------------
-//Работает для одного товара, a с несколькими одновременно у меня не получается
-
 {
-    const arrGood = [
-        ['apple', 20, 5],
-        ['green', 50, 10],
-    ];
-    console.log(arrGood);
-
     const cart = {
+        //items = пустой массив - это товары
+        //totalPrice = 0 - общая стоимость корзины
+        //count = 0 - количество товаров 
         items: [],
         totalPrice: 0,
         count: 0,
 
+        //получить общую стоимость товаров, метод возвращает значение свойства totalPrice
         getTotalPrice() {
+            console.log(this.totalPrice);
             return this.totalPrice;
         },
 
-        //Не понимаю как работать с несколькими товарами, как их добавить и перебирать?
+        // add - добавить товар
+        // Принимает три параметра:
+        // название товара
+        // цену товара
+        // количество товара (опциональный параметр, по умолчанию 1 товар)
+        // этот метод формирует объект из полученных параметров и добавляет его в свойство items
+        // так же вызывает все необходимые методы чтобы свойства count и totalPrice были актуальные
 
-        add([[nameItem, priceItem, countItem = 1]]) {
+        add(nameItem, priceItem, countItem = 1) {
+            this.inceaseCount();
+            this.items.push({
+                name: nameItem,
+                price: priceItem,
+                count: countItem,
+            });
 
-            const inceaseCount = (number) => {
-                this.count = countItem + number;
-                return this.count;
-            };
+            this.inceaseCount();
+            this.calculateItemPrice();
+        },
 
-            //добавила цикл гоняется один и тот же массив
-            for (let i = 0; i < arrGood.length; i++) {
-                arrGood[i] = {
-                    nameItem,
-                    priceItem,
-                    countItem: inceaseCount(10),//Как добавлять это число inceaseCount извне объекта для каждого товара отдельно?
-                };
+        // increaseCount - увеличить количество товаров
+        // Принимает один параметр(число)
+        // Увеличивает свойство count на это число
+        inceaseCount(number) {
+            return number + this.count;
+        },
 
-                this.items.push(arrGood[i]);
-            }
-
-            const calculateItemPrice = () => {
-                let totalPriceItem = priceItem * countItem;
-
-                this.totalPrice = this.totalPrice + totalPriceItem;
-                return this.totalPrice;
-            };
-            calculateItemPrice();
+        // calculateItemPrice - посчитать общую стоимость товаров
+        //пересчитывает стоимость всей корзины и записывает значение в totalPrice 
+        calculateItemPrice() {
+            this.totalPrice = this.items.reduce(((acc, item) => {
+                return acc + item.count * item.price;
+            }), 0)
         },
 
         clear() {
@@ -108,18 +56,19 @@
             this.count = 0;
         },
         print() {
-            let string = JSON.stringify(this.items);
-            console.log(string);
+            let stringJson = JSON.stringify(this.items);
+            console.log(stringJson);
             console.log(this.totalPrice);
         },
     };
 
-    cart.add(arrGood);
-    //видит длину только одного товара, не знаю как сделать массив из этих товаров
-    console.log(cart.add.length);
+    const arrGood = [
+        ['apple', 20, 5],
+        ['green', 50, 10],
+    ];
+    console.log(arrGood);
 
-    console.log('items', cart.items);
-    console.log('count', cart.count);
+    arrGood.forEach(item => cart.add(...item))
     //cart.clear();
     cart.print();
 }
