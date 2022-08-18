c148d2f23c279546dc3a683d6d23a8b659dd428c
Alla Berkana <allaberkana@gmail.com>
2022-08-18 13:58:46 +0300

    lesson 10

---
+++ lesson_10/cart.js
@@ -0,0 +1,70 @@
+'use strict'
+/*Обнаружена уязвимость в нашем функционале
+Если после добавления последнего товара присвоить к totalPrice любое значение,
+например
+cart.totalPrice = 10;
+то при выводе print() общая стоимость корзины будет равна 10
+Чтобы это предотвратить, необходимо свойство totalPrice сделать геттером который будет возвращать результат вызова метода calculateItemPrice
+метод getTotalPrice больше не нужен
+calculateItemPrice переделать таким образом, чтобы сумму он возвращал, а не записывал в свойство totalPrice
+Вызов метода calculateItemPrice оставить только у геттера totalPrice
+*/
+
+{
+    const cart = {
+        items: [],
+        count: 0,
+
+        get totalPrice() {
+            return this.calculateItemPrice();
+        },
+
+        set totalPrice(val) {
+            val = this.calculateItemPrice();
+            console.log(val);
+        },
+
+        add(nameItem, priceItem, countItem = 1) {
+            this.items.push({
+                name: nameItem,
+                price: priceItem,
+                count: countItem,
+            });
+
+            this.inceaseCount();
+        },
+
+        inceaseCount(number) {
+            return number + this.count;
+        },
+
+        calculateItemPrice() {
+            let totalPrice = this.items.reduce(((acc, item) => {
+                return acc + item.count * item.price;
+            }), 0);
+            return totalPrice;
+        },
+
+        clear() {
+            this.items = [];
+            this.totalPrice = 0;
+            this.count = 0;
+        },
+        print() {
+            let stringJson = JSON.stringify(this.items);
+            console.log(stringJson);
+            console.log(this.totalPrice);
+        },
+    };
+
+    const arrGood = [
+        ['apple', 20, 5],
+        ['green', 50, 10],
+    ];
+    console.log(arrGood);
+
+    arrGood.forEach(item => cart.add(...item))
+    //cart.clear();
+    cart.totalPrice = 10;
+    cart.print();
+}
---
+++ lesson_10/script.js
@@ -0,0 +1,28 @@
+'use script'
+import { listIPv4 } from './ipv4.js';
+
+//Вам дан список ip-адресов -> IPv4
+//задача: написать функцию, которая будет возвращать число равное количеству уникальных ip-адресов
+
+//[... new Set(arr)]
+{
+    const getSetCol = (arr) => {
+        const uniqVals = [... new Set(arr)];
+        const number = uniqVals.length;
+        return number;
+    };
+    const numberVals = getSetCol(listIPv4);
+    console.log('numberVals: ', numberVals);
+}
+
+//Array.from(new Set(arr))
+{
+
+    const getSetCol = (arr) => {
+        const uniqVals = Array.from(new Set(arr));
+        const number = uniqVals.length;
+        return number;
+    };
+    const numberVals = getSetCol(listIPv4);
+    console.log('numberVals: ', numberVals);
+}
