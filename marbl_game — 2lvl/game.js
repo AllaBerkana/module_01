3b959558289b13efde47b58bee41b65c00bdf0bb
Alla Berkana <allaberkana@gmail.com>
2022-09-13 23:32:19 +0300

    edit prompt 2lvl

--- marbl_game — 2lvl/game.js
+++ marbl_game — 2lvl/game.js
@@ -15,7 +15,8 @@
     // check for isNaN and from 1 to result.player
     const checkPrintNumber = () => {
       if (Number.isNaN(userPrint) || !Number.isFinite(userPrint)) {
-        userPrint = parseFloat(prompt('Введите число!'));
+        userPrint = parseFloat(prompt(`
+        Введите число от 1 до ${result.player}!`));
       } else if (!(userPrint >= 1 && userPrint <= result.player)) {
         userPrint = parseFloat(prompt(
           `Введите число от 1 до ${result.player}!`));
@@ -104,14 +105,26 @@
           return result;
         };
 
-        userStep();
-        computerStep();
+        if (!(result.player <= 0 || result.computer <= 0)) {
+          userStep();
+        } else {
+          return alert(`Игра окончена.
+          Счёт Игрок ${result.player}: Компьютер ${result.computer}`);
+        }
+
+        if (!(result.player <= 0 || result.computer <= 0)) {
+          computerStep();
+        } else {
+          return alert(`Игра окончена.
+          Счёт Игрок ${result.player}: Компьютер ${result.computer}`);
+        }
 
         if (result.player <= 0 || result.computer <= 0) {
           return alert(`Игра окончена.
                     Счёт Игрок ${result.player}: Компьютер ${result.computer}`);
         } else {
-          userPrint = parseFloat(prompt('Введите число'));
+          userPrint = parseFloat(prompt(`
+          Введите число от 1 до ${result.player}!`));
           return game();
         }
       }
